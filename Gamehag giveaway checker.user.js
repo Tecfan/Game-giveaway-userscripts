b735554787ce315bcb98bf791ef88f4dcb85c7bc
Tecfan <palalvsaker@gmail.com>
2020-04-30 21:25:55 +0200

    Update Gamehag giveaway checker.user.js
    Added 1 more failed giveaway
    Changed background color style for link to next giveaway
    Fixed some logic in checking for "mobile" error message (script failed on giveaway/756 because html structure was slightly different)

--- Gamehag giveaway checker.user.js
+++ Gamehag giveaway checker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gamehag giveaway checker
 // @namespace    http://tampermonkey.net/
-// @version      1.003
+// @version      1.004
 // @updateURL    https://github.com/Tecfan/gamehaggwchecker/raw/master/Gamehag%20giveaway%20checker.user.js
 // @downloadURL  https://github.com/Tecfan/gamehaggwchecker/raw/master/Gamehag%20giveaway%20checker.user.js
 // @description  Check old Gamehag giweaways if they are still active.
@@ -20,8 +20,8 @@
     /* Array with all the bad giveaway pages that look like they will work, but has manually been tested to not work.
        They do not work either because it gives the error: "No more keys left", or because one of the tasks does not
        work (like joining the Gamehag Steam group), or because it's a shitty in-game item code which is not worth our time. */
-    var errorGws = [123, 125, 128, 131, 234, 258, 295, 321, 338, 343, 353, 365, 533, 535, 541, 549, 553, 554, 584, 586, 588, 
-                   636, 670, 692, 693, 696, 705, 712, 716, 718, 719, 733, 744, 785];
+    var errorGws = [123, 125, 128, 131, 234, 258, 295, 321, 338, 343, 353, 365, 533, 535, 541, 549, 553, 554,
+                    584, 586, 588, 636, 670, 692, 693, 696, 705, 712, 716, 718, 719, 733, 744, 748, 785];
 
     // Declaring some for loop variables because for some reason Gamehag.com doesn't allow me to declare them inside the loops in my script.
     var z = 0
@@ -66,11 +66,13 @@
     }
 
     /* Check if the giveaway includes an error message that says that the giveaway can only be completed
-       on a mobile phone. onlyMobile will be either true or false if the error message exists            */
+       on a mobile phone. onlyMobile is true if the error exists, and is set to false if it doesn't exist. */
     var mobile = document.querySelectorAll("div.single-giveaway-task > div.task-content > a");
     var onlyMobile = document.querySelector("div.giveaway-content > div.alert.alert-warning.text-center");
     if (onlyMobile) {
         onlyMobile = onlyMobile.innerText.includes("only on our mobile");
+    } else {
+        onlyMobile = false;
     }
 
     // Check if the user wants to skip completed giveaways or not.
@@ -81,13 +83,11 @@
             // Not sure why I'm doing it like this. Any suggestions?
             alreadyComplete = alreadyComplete.getAttribute("style");
         }
-    }
-    // If setting is set to 0, stop the script and post a message to the user.
+    }// If setting is set to 0, stop the script and post a message to the user.
     else if (skipCompleted === 0) {
         errorMesssageHeader.insertAdjacentHTML("beforeend", '<br><span style="background-color: red; font-size: 60%;">Giveaway possibly functional,<br>but you have already completed it.<br>Script stopped due to your "skipCompleted" setting.<\/span>');
         return console.log('Giveaway possibly functional, but you have already completed it. Script stopped due to your "skipCompleted" setting.');
-    }
-    // If it's set wrongly, return an error message an stop the script.
+    }// If it's set wrongly, return an error message an stop the script.
     else {
         errorMesssageHeader.insertAdjacentHTML("beforeend", '<br><span style="background-color: red; font-size: 60%;">Error with "skipCompleted" variable.<br>see the first line in the userscript.<br>Value should be either "0" or "1".<\/span>');
         return console.log('Error with "skipCompleted" variable, see the first line in the userscript. Value should be either "0" or "1"');
@@ -96,7 +96,7 @@
     /* Insert link to next giveaway page so that you don't have to edit URL manually when the script stops.
        Opens in new tab so that the userscript starts properly. Any other way of making sure the userscript
        will run after a normal URL click?                                                                  */
-    document.querySelector("div.giveaway-games > h3").insertAdjacentHTML("beforebegin", `<h3 class="text-xs-center mb-3" style="background-color: red;"> <a href="https://gamehag.com/giveaway/${x}" target="_blank">Next giveaway</a></h3>`);
+    document.querySelector("div.giveaway-games > h3").insertAdjacentHTML("beforebegin", `<h3 class="text-xs-center mb-3"> <a href="https://gamehag.com/giveaway/${x}" target="_blank" style="background-color: red;">Next giveaway</a></h3>`);
 
     // if there is a giveaway counter on the page, start the script
     if (document.getElementsByClassName("giveaway-counter")[0]) {
@@ -123,21 +123,18 @@
             for (divelem = 0; divelem < gwEnded.length; divelem++) {
                 if (gwEnded[divelem].innerText.includes("not available in your country") || gwEnded[divelem].innerText.includes("has ended")) {
                     return openNextPage();
-                }
-                /* Or else, check if the giveaway is in the future and abort the script (not sure if this will ever be true) */
+                }/* Or else, check if the giveaway is in the future and abort the script (not sure if this will ever be true) */
                 else if (gwEnded[divelem].innerText.includes("Giveaway has not yet taken off")) {
                     return endOfScript();
                 }
             }
 
             return console.log("This giveaway is probably still active");
-        }
-        /* If the main arguments are not true, and a message saying that the giveaway is in the future
+        }/* If the main arguments are not true, and a message saying that the giveaway is in the future
            is visible, then abort the script.                                                          */
         else if (gwEnded[1].innerText.includes("Giveaway has not yet taken off")) {
             return endOfScript();
-        }
-        // In any other case, the giveaway is surely not active, so open the next giveaway and restart the script by reloading the page.
+        }// In any other case, the giveaway is surely not active, so open the next giveaway and restart the script by reloading the page.
         else {
             return openNextPage();
         }
