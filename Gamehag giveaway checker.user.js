413346849727ad455dd7b3f20f744fa991eb95e4
Tecfan <palalvsaker@gmail.com>
2020-04-30 18:32:33 +0200

    Update Gamehag giveaway checker.user.js
    More bad giveaway's and red background color for the next giveaway link

--- Gamehag giveaway checker.user.js
+++ Gamehag giveaway checker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gamehag giveaway checker
 // @namespace    http://tampermonkey.net/
-// @version      1.002
+// @version      1.003
 // @updateURL    https://github.com/Tecfan/gamehaggwchecker/raw/master/Gamehag%20giveaway%20checker.user.js
 // @downloadURL  https://github.com/Tecfan/gamehaggwchecker/raw/master/Gamehag%20giveaway%20checker.user.js
 // @description  Check old Gamehag giweaways if they are still active.
@@ -21,9 +21,9 @@
        They do not work either because it gives the error: "No more keys left", or because one of the tasks does not
        work (like joining the Gamehag Steam group), or because it's a shitty in-game item code which is not worth our time. */
     var errorGws = [123, 125, 128, 131, 234, 258, 295, 321, 338, 343, 353, 365, 533, 535, 541, 549, 553, 554, 584, 586, 588, 
-                   636, 670, 692, 693, 696, 705, 712, 716, 718, 785];
+                   636, 670, 692, 693, 696, 705, 712, 716, 718, 719, 733, 744, 785];
 
-    // Declaring some variables because for some reason Gamehag.com doesn't allow me to declare them inside the loops in my script.
+    // Declaring some for loop variables because for some reason Gamehag.com doesn't allow me to declare them inside the loops in my script.
     var z = 0
       , aelem = 0
       , divelem = 0;
@@ -96,7 +96,7 @@
     /* Insert link to next giveaway page so that you don't have to edit URL manually when the script stops.
        Opens in new tab so that the userscript starts properly. Any other way of making sure the userscript
        will run after a normal URL click?                                                                  */
-    document.querySelector("div.giveaway-games > h3").insertAdjacentHTML("beforebegin", `<h3 class="text-xs-center mb-3"> <a href="https://gamehag.com/giveaway/${x}" target="_blank">Next giveaway</a></h3>`);
+    document.querySelector("div.giveaway-games > h3").insertAdjacentHTML("beforebegin", `<h3 class="text-xs-center mb-3" style="background-color: red;"> <a href="https://gamehag.com/giveaway/${x}" target="_blank">Next giveaway</a></h3>`);
 
     // if there is a giveaway counter on the page, start the script
     if (document.getElementsByClassName("giveaway-counter")[0]) {
